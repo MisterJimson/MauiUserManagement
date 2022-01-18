ba5893651819e0461b1ea3807b040a66078c7cd4
Jason Rai <jason.c.rai@gmail.com>
2022-01-17 20:57:58 -0500

    feat: binding and some more controls

--- Pages/SignInPage.xaml.cs
+++ Pages/SignInPage.xaml.cs
@@ -1,26 +1,64 @@
-using System;
-using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
-using Supabase;
-using Supabase.Gotrue;
 using Client = Supabase.Client;
-using Newtonsoft.Json;
-using System.Diagnostics;
-
+using System.Runtime.CompilerServices;
 
 namespace MauiUserManagement
 {
 	public partial class SignInPage : ContentPage
 	{
+		private string email;
+		public string Email
+		{
+			get => email;
+			set => SetField(ref email, value);
+		}
+
+		private string password;
+		public string Password
+		{
+			get => password;
+			set => SetField(ref password, value);
+		}
+
+		private string passwordConfirm;
+		public string PasswordConfirm
+		{
+			get => passwordConfirm;
+			set => SetField(ref passwordConfirm, value);
+		}
+
+		private string passwordError;
+		public string PasswordError
+		{
+			get => passwordError;
+			set => SetField(ref passwordError, value);
+		}
+
 		public SignInPage()
 		{
 			InitializeComponent();
+			BindingContext = this;
 		}
 
 		async void OnSignInClicked(object sender, EventArgs e)
         {
-			await Client.Instance.Auth.SendMagicLink(EmailEntry.Text);
+			try
+			{
+				var test = Client.Instance.Auth; 
+				var session1 = await Client.Instance.Auth.SignUp(Email, Password);
+				//var session2 = await Client.Instance.Auth.SignIn(Email, Password);
+				Console.WriteLine(session1.AccessToken);
+			} catch (Exception ex)
+            {
+				Console.WriteLine(ex.Message);
+            }
+		}
+
+		protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(propertyName);
+			return true;
 		}
 	}
 }
